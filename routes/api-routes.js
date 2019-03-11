79d299bab6da6cdb8a66e958f2e65c6ab4c8bc26
Hannah Schuelke <hannah.r.schuelke@gmail.com>
2019-03-11 10:57:44 -0500

    html routes added

---
+++ routes/api-routes.js
@@ -0,0 +1,14 @@
+// post, new user
+// delete user
+
+// post new event
+// put, for updating events
+// get, for events
+
+// Newsfeed: (nice to haves) 
+// push, add event to user newsfeed
+// put/destroy, remove event from newsfeed
+
+
+
+
