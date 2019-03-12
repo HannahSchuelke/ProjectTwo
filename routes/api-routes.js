26047abb0067f6bd1085db5eec79465a58b138d8
Jake S <45572826+jakescheele@users.noreply.github.com>
2019-03-12 13:34:18 -0500

    JWT Routes created

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,112 +1,145 @@
 // This file is for routes: displaying and saving data to the db
 
 // Dependencies 
-// var db = require("../models"); //Comment back in later (testing API)
+const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+const db = require("../models");
 const https = require("https");
 const axios = require("axios");
 
 // Routes
-module.exports = function (app) {
 
-    // ------- USERS --------
-
-
-    // POST, create a new user
-    app.post("/api/new", function (req, res) {
-        console.log(req.body);
-        db.user.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password //authenication key = will store hash password, not string password. like encryption. 
-        }).then(function (results) {
-            res.end();
-        });
-
-    });
-
-
-    // --------- EVENTS ----------
-
-    // GET, all events
-    app.get("/api/event", function (req, res) {
-        db.Event.findAll({}).then(function (results) {
-            res.json(results);
-        });
-    });
-
-    // POST, new event
-    app.post("/api/event", function (req, res) {
-        db.Event.create(req.body).then(function (results) {
-            res.json(results);
-        });
-    });
-
-    // PUT, for updating events
-    app.put("/api/event", function (req, res) {
-        db.Event.update(
-            req.body,
-            {
-                where: {
-                    id: req.body.id
-                }
-            }).then(function (results) {
-                res.json(results);
-            });
-    });
-    // --------PREDICT HQ API (to search by area)------- (if song kick comes through -- boot this)
-
-    app.get("/api/predictHQ", async (req, res) => {
-        // const config = {
-        //     "Authorization": "0akqMCro5pKNYGxlIdwORyhdvUBntq"
-        // }       
-
-        const token = "0akqMCro5pKNYGxlIdwORyhdvUBntq";
-
-        const config = {
-            headers: { 'Authorization': "Bearer " + token }
-        }
-
-        // const agent = new https.Agent(config);
-
-        const url = "https://api.predicthq.com/v1/events/?place.scope=MSP"
-
-        try {
-            const predictHQData = await axios.get(url, config);
-
-            console.log(predictHQData.data.results);
-            const eventsInArea = predictHQData.data.results;
-
-            for (let i = 0; i < eventsInArea.length; i++) {
-                const artist = eventsInArea[i].title;
-
-                // --------BANDS IN TOWN API (to search by concert)-------
-
-                let bandInfo = await axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=oisacfioqwuuwfcenqou");
-                bandInfo = bandInfo.data[0];
-
-                console.log("bandinfo: ", bandInfo);
-            }
-
-            // you might need to look into promise.all for async (may not work with for loop)
-            res.json("predictHQData")
-
-        } catch (error) {
+// login users
+router.post("/login", function(req, res) {
+    // Fetch user from db
+    db.User.findOne({
+        where: {
+            email: req.body.email
         }
     })
+})
+    .then(function(result) {
+        if (!result)  return  res.SendStatus(404).send();  //user wasn't found
 
-    // ------- NEWSFEED ---------- 
+        // check if user/password is correct
+        else if (result.password === req.body.password) {
+            let userDetails = {
+                email: result.email,
+                id: result.id,
+            };
+            return jwt.sign(userDetails, process.env.JWT_SECRET_KEY, {algorithm: "HS256"}, function(err, token) {
+                if (err) return res.sendStatus(500).json(err)
+                res.json({
+                    user: userDetails,
+                    token: token,
+                })
+            })
+        }
+        else {
+            res.sendStatus(401).send(); // wrong password
+        }
+    })
+    .catch(function(err) {
+        return res.sendStatus(500).json(err)
+    })
 
-    // GET, add event & users (who are also going) to newsfeed
-    app.get("/api/attendee/:id", function (req, res) {
-        // 2; Add a join to include all of the Author's Posts here
-        db.Attendee.findAll({
-            // can put include before or after the way, but not in where
-            include: [{ model: db.attendee }],
+// POST, create a new user
+router.post("/user/new", function (req, res) {
+    console.log(req.body);
+    db.user.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password //authenication key = will store hash password, not string password. like encryption. 
+    }).then(function (results) {
+        res.end();
+    });
+
+});
+
+
+// --------- EVENTS ----------
+
+// GET, all events
+router.get("/api/event", function (req, res) {
+    db.Event.findAll({}).then(function (results) {
+        res.json(results);
+    });
+});
+
+// POST, new event
+router.post("/api/event", function (req, res) {
+    db.Event.create(req.body).then(function (results) {
+        res.redirect
+        res.json(results);
+    });
+});
+
+// PUT, for updating events
+router.put("/api/event", function (req, res) {
+    db.Event.update(
+        req.body,
+        {
             where: {
-                id: req.params.id
+                id: req.body.id
             }
         }).then(function (results) {
             res.json(results);
         });
+});
+// --------PREDICT HQ API (to search by area)------- (if song kick comes through -- boot this)
+
+router.get("/api/predictHQ", async (req, res) => {
+    // const config = {
+    //     "Authorization": "0akqMCro5pKNYGxlIdwORyhdvUBntq"
+    // }       
+
+    const token = "0akqMCro5pKNYGxlIdwORyhdvUBntq";
+
+    const config = {
+        headers: { 'Authorization': "Bearer " + token }
+    }
+
+    // const agent = new https.Agent(config);
+
+    const url = "https://api.predicthq.com/v1/events/?place.scope=MSP"
+
+    try {
+        const predictHQData = await axios.get(url, config);
+
+        console.log(predictHQData.data.results);
+        const eventsInArea = predictHQData.data.results;
+
+        for (let i = 0; i < eventsInArea.length; i++) {
+            const artist = eventsInArea[i].title;
+
+            // --------BANDS IN TOWN API (to search by concert)-------
+
+            let bandInfo = await axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?router_id=oisacfioqwuuwfcenqou");
+            bandInfo = bandInfo.data[0];
+
+            console.log("bandinfo: ", bandInfo);
+        }
+
+        // you might need to look into promise.all for async (may not work with for loop)
+        res.json("predictHQData")
+
+    } catch (error) {
+    }
+})
+
+// ------- NEWSFEED ---------- 
+
+// GET, add event & users (who are also going) to newsfeed
+router.get("/api/attendee/:id", function (req, res) {
+    // 2; Add a join to include all of the Author's Posts here
+    db.Attendee.findAll({
+        // can put include before or after the way, but not in where
+        include: [{ model: db.attendee }],
+        where: {
+            id: req.params.id
+        }
+    }).then(function (results) {
+        res.json(results);
     });
+});
 }
