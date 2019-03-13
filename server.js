323932a2555d3dde29984a9e720d17dbbafabd65
Jake S <45572826+jakescheele@users.noreply.github.com>
2019-03-13 09:44:00 -0500

    Updates

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 ////////// DEPENDENCIES
 
 var express = require('express');
-var jwt_express = requuire('express-jwt');
+var jwt_express = require('express-jwt');
+require('dotenv').config();
 
 ///////// INITIALIZE EXPRESS
 
@@ -17,9 +18,9 @@ app.use(express.json());
 // STATIC DIRECTORY
 app.use(express.static('public'));
 
-
+console.log(process.env.JWT_SECRET_KEY)
 //tell express to use JSON WebTokens. JWT-Express will autofill req.user with the user details
-app.use(jwt_express({ secret: JWT_SECRET_KEY }).unless({ path: ['/token', '/favicon.ico'] }));
+app.use(jwt_express({ secret: process.env.JWT_SECRET_KEY }).unless({ path: ['/token', '/favicon.ico'] }));
 
 
 /////////// ROUTES
