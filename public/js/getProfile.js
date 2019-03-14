d95715d457d31a27f2e994a607ed7ed3121faa2f
Jake S <45572826+jakescheele@users.noreply.github.com>
2019-03-14 18:20:29 -0500

    Can create new event and join table record

--- public/js/getProfile.js
+++ public/js/getProfile.js
@@ -1,14 +1,21 @@
+// import { userInfo } from "os";
+
 // FUNCTION FOR PROFILE EVENTS
+console.log(localStorage.getItem(user.id))
 
 $.ajax({
-    url: '/api/events/user',
+    url: '/api/events/'+localStorage.getItem(user.id),
     method: 'get'
 })
     .then(function (response) {
         for (i in response) {
-            $('#').append(
-                // html for one event
-                // response[i].title
+            $('#userEvent').append(
+                `<div class='event'>
+                    <div id='event-title' class='event-item'>${response[i].title}</div>
+                    <div id='event-date' class='event-item'>${response[i].date}</div>
+                    <div id='event-location' class='event-item'>${response[i].location}</div>
+                    <div id='event-artist' class='event-item'>${response[i].artist}</div>
+                </div>`
             )
         }
     })
