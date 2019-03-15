92020fa8c4f7d41ad4c0255a259a093213975b5a
Jake S <45572826+jakescheele@users.noreply.github.com>
2019-03-14 22:59:05 -0500

    Newsfeed populates from events table

--- public/js/getFeed.js
+++ public/js/getFeed.js
@@ -6,14 +6,18 @@
 
 $.ajax({
     url: '/api/events',
-    data: '',
     method: 'get'
 })
     .then(function (response) {
+        console.log(response)
         for (i in response) {
-            $('#').append(
-                // html for one event
-                // response[i].title
+            $('#feed').append(
+                `<div class='event'>
+                    <div id='event-title' class='event-item'>${response[i].title}</div>
+                    <div id='event-date' class='event-item'>${response[i].date}</div>
+                    <div id='event-location' class='event-item'>${response[i].location}</div>
+                    <div id='event-artist' class='event-item'>${response[i].artist}</div>
+                </div>`
             )
         }
     })
