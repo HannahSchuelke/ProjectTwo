1fb732837c64c879dd0ae28cd7e7f59e24778f25
Jake S <45572826+jakescheele@users.noreply.github.com>
2019-03-14 22:30:52 -0500

    Posting multiple times works from profile

--- public/js/newEvent.js
+++ public/js/newEvent.js
@@ -7,18 +7,18 @@
 
 // post and create new event
 $(document).ready(function() {
-    $('#insertForm').on('submit', function() {
+    $('#formSubmit').on('click', function() {
           var body = {
-            title: $('#event-title').val(),
-            date: $('#event-date').val(),
-            location: $('#event-location').val(),
-            artist: $('#event-artist').val(),
+            title: $('#title').val(),
+            date: $('#date').val(),
+            location: $('#location').val(),
+            artist: $('#artist').val(),
         }
         console.log("Body to be sent: " + body)
         $.ajax({
             url: "/api/event/new",
             data: body,
-            method: "post"
+            method: "post",
         })
         .then(function(response) {
             console.log("Response"+response)
